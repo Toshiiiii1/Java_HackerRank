14e148ce556ca98c569cd89c99d1383aff100360
Toshiiiii1 <106165329+Toshiiiii1@users.noreply.github.com>
2022-10-12 10:05:59 +0700

    Type: OOP | Level: Easy

---
+++ Method_Overriding/Soccer.java
@@ -0,0 +1,11 @@
+package Method_Overriding;
+
+class Soccer extends Sports{
+    String getName(){
+        return "Soccer Class";
+    }
+
+    void getNumberOfTeamMembers() {
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+}
---
+++ Method_Overriding/Sports.java
@@ -0,0 +1,11 @@
+package Method_Overriding;
+
+class Sports {
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
